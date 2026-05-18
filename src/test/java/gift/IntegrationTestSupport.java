5833a72e845067c35d93b3db19e2b3058afe49fa
문준용 <moon.junyong@postype.com>
2026-05-18 23:19:08 +0900

    docs(test): 통합 테스트 베이스의 DB 격리 의도 한 줄 주석 추가

--- src/test/java/gift/IntegrationTestSupport.java
+++ src/test/java/gift/IntegrationTestSupport.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+// 모든 통합 테스트의 베이스. 매 테스트 전에 DB 를 truncate 해 테스트 간 상태 공유를 차단한다.
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class IntegrationTestSupport {
