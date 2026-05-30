37a694a5dfa14cdb94a93d334c7b36de5939a797
문준용 <moon.junyong@postype.com>
2026-05-30 11:03:49 +0900

    refactor(support): 클라이언트 오류 로그 수준 통일

--- src/main/java/gift/support/error/ErrorType.java
+++ src/main/java/gift/support/error/ErrorType.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "E400", "잘못된 요청입니다", LogLevel.INFO),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "E401", "인증이 필요합니다", LogLevel.INFO),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "E403", "권한이 없습니다", LogLevel.WARN),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "E403", "권한이 없습니다", LogLevel.INFO),
     NOT_FOUND(HttpStatus.NOT_FOUND, "E404", "리소스를 찾을 수 없습니다", LogLevel.INFO),
     CONFLICT(HttpStatus.CONFLICT, "E409", "요청이 현재 상태와 충돌합니다", LogLevel.INFO),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E500", "서버 내부 오류가 발생했습니다", LogLevel.ERROR);
